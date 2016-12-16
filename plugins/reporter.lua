6cfe02729583f12790921a82c79b2e82fdabbe72
abolfazlabbasifx <mycarphone001@gmail.com>
2016-12-16 15:03:48 +0330

    Tele

--- plugins/reporter.lua
+++ plugins/reporter.lua
@@ -1,15 +1,15 @@
 local function run(msg,matches)
 if matches[1] == "add" and is_admin1(msg) then
-local textadd = "\n✽Status:»One Group Added!« \n➖➖➖➖➖\nAdder Info: \n》Name Adder: "..(msg.from.print_name or '----').." \n》UserName: @"..(msg.from.username or 'permag').."\n》ID: "..(msg.from.id or '----').." \n➖➖➖➖➖\nGroup Info:  \n●Name: "..string.gsub(msg.to.print_name, "_", " ").." \n●ID : "..(msg.to.id or '----').." \n➖➖➖➖➖\nFor Leave Bot Send:\n/leave "..(msg.to.id or '----').."\n➖➖➖➖➖\n@teleguardchannel"
+local textadd = "\n✽Status:»ربات در گروه ادد شد!« \n➖➖➖➖➖\nمشخصات ادد کننده : \n》نام: "..(msg.from.print_name or '----').." \n》یوزرنیم: @"..(msg.from.username or 'teleguardchannel').."\n》آیدی: "..(msg.from.id or '----').." \n➖➖➖➖➖\nمشخصات گروه:  \n● نام: "..string.gsub(msg.to.print_name, "_", " ").." \n●آیدی : "..(msg.to.id or '----').." \n➖➖➖➖➖\n برای خارج شدن ربات از گروه دستور زیر را ارسال کنید:\n/leave "..(msg.to.id or '----').."\n➖➖➖➖➖\n@teleguardchannel"
 
 
-send_large_msg("user#id118389682",textadd)
+send_large_msg("user#id149553519",textadd)
 
 end
 if matches[1] == "rem" and is_admin1(msg) then
 
-local textrem = "\n✽Status:»One Group Removed!« \n➖➖➖➖➖\nRemover Info:  \n》Name Remover: "..(msg.from.print_name or '----').." \n》UserName: @"..(msg.from.username or 'permag').."\n》ID: "..(msg.from.id or '----').." \n➖➖➖➖➖\nGroup Info:  \n●Name: "..string.gsub(msg.to.print_name, "_", " ").." \n●ID : "..(msg.to.id or '----').." \n➖➖➖➖➖\n@teleguardchannel"
-send_large_msg("user#id118389682",textrem)
+local textrem = "\n✽Status:» ربات از گروه اخراج شد!« \n➖➖➖➖➖\nمشخصات ریمو کننده:  \n》نام: "..(msg.from.print_name or '----').." \n》یوزرنیم: @"..(msg.from.username or 'teleguardchannel').."\n》آیدی: "..(msg.from.id or '----').." \n➖➖➖➖➖\n مشخصات گروه:  \n●نام: "..string.gsub(msg.to.print_name, "_", " ").." \n●آیدی : "..(msg.to.id or '----').." \n➖➖➖➖➖\n@teleguardchannel"
+send_large_msg("user#id149553519",textrem)
 end
 end
 return {
